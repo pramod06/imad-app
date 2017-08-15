28c0f659ca73def4095478bd8184b571bf1c2af7
pramod06 <30710747+pramod06@users.noreply.github.com>
2017-08-15 09:14:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,12 @@
 console.log('Loaded!');
 
+// change some text
 element = document.getElementById('newText');
 
 element.innerHTML = 'This is the new modified text';
+
+// move image
+var img = document.getElementById('madi');
+img.onClick = function(){
+    img.style.marginLeft = '100px';
+}
