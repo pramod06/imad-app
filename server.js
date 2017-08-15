c82a344ab0592ac604ba123f6853555cd3db7b7d
pramod06 <30710747+pramod06@users.noreply.github.com>
2017-08-15 14:24:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/count', function(req, res) {
+    counter = counter + 1;
+    res.send(counter, toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-var counter = 0;
-app.get('/counter', function(req, res) {
-    counter = counter + 1;
-    res.send(counter, toString());
-});
 
 app.get('/article_one', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));   
